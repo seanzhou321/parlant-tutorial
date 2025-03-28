ef5a7418bcac09a4e895e09a7d72b19d888ab832
seanzhou321 <sean.zhou321@gmail.com>
2025-03-28 16:10:35 -0700

    update the setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,16 @@
-pip -m venv venv
+python -m venv venv
 call venv\Scripts\activate.bat
+
+pip install parlant
+# export OPENAI_API_KEY=your-api-key
+
+pip install parlant-client
+
+pip install poetry
+cd basic-client
+poetry install
+cd ..
+
+cd tool-service
+poetry install
+cd ..
